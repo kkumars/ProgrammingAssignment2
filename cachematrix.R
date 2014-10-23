6611af6d3465ee471e057866146f4bcf57660f66
kkumars <kamlesh.isi@gmail.com>
2014-10-23 10:37:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 #rprog-008
 #kamlesh.isi@gmail.com
 
-### The first function, makeVector creates a special "Matrix", 
+### The first function, makeCacheMatrix creates a special "Matrix", 
 ### which is really a list containing a function to
 ### 1. set the value of the matrix
 ### 2. get the value of the matrix
@@ -24,8 +24,8 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
-### The following function calculates the mean of the special "matrix"
-### created with the above function. However, it first checks to see if
+### The cacheSolve function calculates the inverse of the special "matrix"
+### created with the makeCacheMatrix function. However, it first checks to see if
 ### the inverse has already been calculated. If so, it gets the inverse 
 ### from the cache and skips the computation. Otherwise, it calculates 
 ### the inverse of the data and sets the value of the inverse in the 
